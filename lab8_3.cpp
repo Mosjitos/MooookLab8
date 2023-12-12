f1541be394c4fb60d01ba9ff6cf2df706928b159
Mosjitos <kamolrat_saeboon@cmu.ac.th>
2023-12-12 16:15:41 +0700

    lab8_3.cpp completed finallly!!

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,33 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int urAge, urHeight, bounty;
+	string charNem;
+	cout << "Enter your age: "; cin >> urAge;
+
+    //check age under than 25y
+	if(urAge<=25){
+		cout << "Enter your height: "; cin >> urHeight;
+		if(urHeight<100) charNem = "Chopper";
+		else if(urHeight>=180)
+        {
+			cout << "Enter your bounty: "; cin >> bounty;
+			if(bounty>1100000000) charNem = "Zoro";
+			else charNem = "Sanji";
+		}
+        else  charNem = "Usopp";
+	}
+    //check age that are around 25y to 60y
+    else if(urAge<=60)
+    {
+		cout << "Enter your bounty: "; cin >> bounty;
+		if(bounty>500000000) charNem = "Jinbe";
+			else charNem = "Franky";
+    //check age over than 60y
+	}else{charNem = "Brook";}
+
+	cout << "Your character = " << charNem;
+
+}
